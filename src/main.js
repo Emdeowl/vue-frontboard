f253dd9b8d258c78de7f606c68cc470f3ac41d15
JaeHyuk <kwon850550@naver.com>
2022-09-09 22:07:15 +0900

    Login,vue에 vuex 적용

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import axios from'axios'
+import store from './vuex/store'
 
 const app=createApp(App)
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$serverUrl = '//localhost:8081'
+app.config.globalProperties.$store=store
 app.use(router).mount('#app')
---
+++ src/vuex/mutations.js
@@ -0,0 +1,14 @@
+import * as types from './mutation_types'
+
+export default{
+    [types.USER_ID](state,userId)
+    {
+        state.userId=userId
+    },
+    [types.ERROR_STATE](state,errorState){
+        state.errorState=errorState
+    },
+    [types.IS_AUTH](state,isAuth){
+        state.IS_AUTH=isAuth
+    }
+}
